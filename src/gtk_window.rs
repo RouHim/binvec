eb50117c091c4acdde9424f312e441bd86742f7e
Rouven Hi <rouven@himmelstein.info>
2022-07-07 10:44:40 +0200

    feat: added hello world example

---
+++ src/gtk_window.rs
@@ -0,0 +1,74 @@
+use glib::subclass::InitializingObject;
+use glib::Object;
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::{gio, glib, Application};
+use gtk::{Button, CompositeTemplate};
+
+// ANCHOR: object
+// Object holding the state
+#[derive(CompositeTemplate, Default)]
+#[template(resource = "/com/rouhim/binvec/window.ui")]
+pub struct Window {
+    #[template_child]
+    pub button: TemplateChild<Button>,
+}
+// ANCHOR_END: object
+
+// ANCHOR: subclass
+// The central trait for subclassing a GObject
+#[glib::object_subclass]
+impl ObjectSubclass for Window {
+    // `NAME` needs to match `class` attribute of template
+    const NAME: &'static str = "BinVec";
+    type Type = AppWindow;
+    type ParentType = gtk::ApplicationWindow;
+
+    fn class_init(klass: &mut Self::Class) {
+        klass.bind_template();
+    }
+
+    fn instance_init(obj: &InitializingObject<Self>) {
+        obj.init_template();
+    }
+}
+// ANCHOR_END: subclass
+
+// ANCHOR: object_impl
+// Trait shared by all GObjects
+impl ObjectImpl for Window {
+    fn constructed(&self, obj: &Self::Type) {
+        // Call "constructed" on parent
+        self.parent_constructed(obj);
+
+        // Connect to "clicked" signal of `button`
+        self.button.connect_clicked(move |button| {
+            // Set the label to "Hello World!" after the button has been clicked on
+            button.set_label("Hello World!");
+        });
+    }
+}
+// ANCHOR_END: object_impl
+
+// Trait shared by all widgets
+impl WidgetImpl for Window {}
+
+// Trait shared by all windows
+impl WindowImpl for Window {}
+
+// Trait shared by all application windows
+impl ApplicationWindowImpl for Window {}
+
+glib::wrapper! {
+    pub struct AppWindow(ObjectSubclass<Window>)
+        @extends gtk::ApplicationWindow, gtk::Window, gtk::Widget,
+        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
+                    gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+}
+
+impl AppWindow {
+    pub fn new(app: &Application) -> Self {
+        // Create new window
+        Object::new(&[("application", app)]).expect("Failed to create Window")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use gtk::prelude::*;
+use gtk::{gio, Application};
+
+mod gtk_window;
+
+const APP_ID: &str = "com.rouhim.binvec";
+
 fn main() {
-    println!("Hello, world!");
+    // Register and include resources
+    gio::resources_register_include!("compiled.gresource").expect("Failed to register resources.");
+
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create new window and present it
+    let window = gtk_window::AppWindow::new(app);
+    window.present();
 }
